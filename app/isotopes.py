cdfac2497acad23e1747eb449ec823e4eeaeb4d1
Kat <katerib@users.noreply.github.com>
2025-02-10 22:03:09 -0500

    Rename src -> app ; add basic flask app files

---
+++ app/isotopes.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Isotope:
+    name: str
+    half_life: float
+    half_life_unit: str                     # s, d, or y
+
+CARBON_14 = Isotope(name="Carbon-14", half_life=5730, half_life_unit='y')
+IODINE_131 = Isotope(name="Iodine-131", half_life=8.02, half_life_unit='d')
+URANIUM_238 = Isotope(name="Uranium-238", half_life=4.468e9, half_life_unit='y')
+BISMUTH_193M = Isotope(name="Bismuth-193m", half_life=3.2, half_life_unit='s')
