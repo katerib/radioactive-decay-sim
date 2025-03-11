8757f77324e0ae8a09bc1e56806e8ae43fe43e7a
bitboody <bodebi639@gmail.com>
2025-03-11 20:11:42 +0200

    Refactor half-life unit processing to improve formatting and remove unnecessary casting

--- app/routes.py
+++ app/routes.py
@@ -99,10 +99,11 @@ def search():
             hl_unit_array = re.split(r'\+|\-|\s+', entry['isotope_data'][i]['half-life'])
 
             if hl_unit_array[1][0].isnumeric():
-                hl_unit_array[0] += f'^{hl_unit_array[1][0]}'
-                hl_unit_array[0] = hl_unit_array[0].replace('×10^', 'e')
-                hl_unit_array[0] = int(hl_unit_array[0])
+                hl_unit_array[0] += hl_unit_array[1][0]
+                hl_unit_array[0] = hl_unit_array[0].replace('×10', 'e')
+                hl_unit_array[0] = hl_unit_array[0]
                 hl_unit_array[1] = hl_unit_array[1].replace(hl_unit_array[1][0], '')
+                print(hl_unit_array)
 
             hl = hl_unit_array[0]
             unit = ''.join(filter(lambda x: x.isalpha(), hl_unit_array[1]))
